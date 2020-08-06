5d84ff56abdc1f7e012491fa915ace8ff134a707
Mariana <moraismariana83@gmail.com>
2020-08-05 21:31:29 -0300

    feat: Start web

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+import Routes from './routes';
+
+// JSX - JavaScript + XML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
